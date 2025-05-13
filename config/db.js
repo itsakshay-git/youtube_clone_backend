8f0a2b0d7edcf078429423f6c73115f666a80b81
itsakshay-git <110621476+itsakshay-git@users.noreply.github.com>
2025-05-13 18:46:59 +0530

    mongo connection setting

--- config/db.js
+++ config/db.js
@@ -15,8 +15,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_ATLAS)
-    await mongoose.connect(process.env.MONGO_URI || process.env.MONGO_ATLAS);
+    await mongoose.connect(process.env.MONGO_ATLAS || process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error);
